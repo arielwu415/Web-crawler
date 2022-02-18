93b7fe8cb752bfa19633f315ae2820eff6a1590f
Clarence-Hugues Domond <73674152+SoftDevCHD@users.noreply.github.com>
2022-02-17 16:57:30 -0800

    Refactored code and added links count to reports#.csv

--- outlinks_count.py
+++ outlinks_count.py
@@ -2,6 +2,6 @@ import csv
 
 
 def write_links_count(link, count, filename):
-    with open(filename, 'w', newline='') as report:
+    with open(filename, 'a', newline='') as report:
         _writer = csv.writer(report)
         _writer.writerow([link, count])
