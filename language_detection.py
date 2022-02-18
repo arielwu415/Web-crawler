2a4156771513ac7cce4dbd97c7399e3c0d8a97e7
arielwu415 <ariel070415@gmail.com>
2022-02-17 23:22:15 -0800

    :sparkles: Added checking html lang attribute feature

--- language_detection.py
+++ language_detection.py
@@ -1,20 +1,36 @@
 from bs4.element import Comment
 from langdetect import detect, DetectorFactory
+from iso639 import languages
 from crawler_bot import get_soup
 
+lang_dict = {"ko":"Korean",
+             "en":"English",
+             "fr":"French"}
 
 def detect_language(soup):
-    # search for <html lang="...">
-    # if attribute exists, return lang value
-    # incomplete
-    DetectorFactory.seed = 0
-
-    # if not exist, detect content text
-    txt = soup.findAll(text=True)
-    visible_txt = filter(tag_visible, txt)  # exclude unneeded tags
+    # check if lang attribute exists
+    if soup.html.has_attr('lang'):
+        # slice the value [0:2]. For example, "ko-KR" get "ko"
+        lang = soup.html['lang'][0:2]
+        
+        # use iso-639 code module to get the language name
+        lang = languages.get(alpha2 = lang).name
+        return lang
     
-    lan = detect(u" ".join(t.strip() for t in visible_txt))
-    return lan
+    # if  attribute does not exist, detect content text
+    else:
+        # fix langdetect's instable results
+        DetectorFactory.seed = 0
+    
+        txt = soup.findAll(text=True)
+        # exclude unneeded tags
+        filtered_txt = filter(tag_visible, txt)
+        
+        visible_txt = str(u" ".join(t.strip() for t in filtered_txt))
+        
+        # use iso-639 code module to get the language name
+        lang = languages.get(alpha2 = detect(visible_txt)).name
+        return lang
 
 
 def tag_visible(element):
@@ -27,4 +43,4 @@ def tag_visible(element):
 
 
 s = get_soup("https://www.coupang.com/")
-print(detect_language(s))
+print("This webstie is in", detect_language(s))
