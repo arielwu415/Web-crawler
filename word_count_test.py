21f50cd20a9a33e22cecd119eb467f39cdf60ee1
vdugo <v3rdugo@yahoo.com>
2022-02-20 07:39:36 -0800

    added some comments

--- word_count_test.py
+++ word_count_test.py
@@ -5,8 +5,13 @@ from word_count import *
 # tuple containing domain names for repository/{domain_directory}
 directories = ('stackoverflow', 'coupang', 'gouvernement')
 
+# gets the word lists for each domain returned as a list of lists
+# e.g. [ ['stack', 'overflow', 'words'], ['coupang', 'words'], ['gouvernement', 'words'] ]
 word_lists = get_wordlists(directories)
 
+# each of these is a dictionary for each domain. 
+# in each dictionary, each key is a unique word
+# and each value is the count of that respective word
 stackoverflow_word_count = get_word_count(word_lists[0])
 coupang_word_count = get_word_count(word_lists[1])
 gouvernement_word_count = get_word_count(word_lists[2])
