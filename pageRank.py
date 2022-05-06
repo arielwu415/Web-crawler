5c7e0771dd41162cef1b87dd2eb0915828599235
arielwu415 <ariel070415@gmail.com>
2022-05-06 01:38:24 -0700

    New crawl

--- pageRank.py
+++ pageRank.py
@@ -7,7 +7,6 @@ Created on Sun May 1 15:27:04 2022
 import csv
 import networkx as nx
 
-
 def create_edge_list(url_nodes):
     index = 0
     for node_set in url_nodes:
@@ -18,4 +17,10 @@ def create_edge_list(url_nodes):
                 with open(filename, 'a', newline='') as report:
                     _writer = csv.writer(report)
                     _writer.writerow([nodes[0], nodes[i]])
-                
+
+# read edge_list file and create a graph G
+with open("edge_list1.csv", "rb") as edges:
+    G = nx.read_edgelist(edges, delimiter=",", encoding="utf-8")
+    
+# make an adjacency matrix
+A = nx.adjacency_matrix(G)
