29f1c09bf934bf0530d376c2ae3455293dd6a2c9
arielwu415 <ariel070415@gmail.com>
2022-05-12 01:17:20 -0700

    :heavy_check_mark: Fixed bugs and add get_top_100

--- pageRank.py
+++ pageRank.py
@@ -9,56 +9,70 @@ import networkx as nx
 import numpy as np
 from outlinks_count import clear_report_file
 
+
 class pageRank:
     
     def __init__(self):
         self.pr = []
+        self.node_list = []
+    
     
     def create_pageRank(self, filename):
         A, nodes = self.read_edge_list(filename)
-        v = np.ones(nodes, 1) / nodes
+        v = np.ones((nodes, 1)) / nodes
         pA = self.probability_matrix(A)
         
         self.pr = self.get_pageRank(pA, v)
     
-    def read_edge_list(filename):
+    
+    def read_edge_list(self, filename):
+        G = nx.DiGraph()
+        
+        all_nodes = ["page{}".format(n+1) for n in range(len(self.node_list))]
+        
         # read edge_list file and create a graph G
-        with open("edge_list1.csv", "rb") as edges:
-            G = nx.read_edgelist(edges, delimiter=",", create_using=nx.DiGraph, encoding="utf-8")
-            
+        with open(filename, "r") as edgecsv:
+            edgereader = csv.reader(edgecsv)
+            edges = [tuple(e) for e in edgereader][:]
+        
+        G.add_nodes_from(all_nodes)
+        G.add_edges_from(edges)
+        
         # make an adjacency matrix
         A = nx.to_numpy_array(G)
-        nodes = G.get_node_number()
-        
-        return A, nodes
-    
-    # A[:, 1] : second column
-    # len(A[:,1]) <-- size of column
+        A = np.transpose(A)
 
-    def probability_matrix(A):
+        nodes = G.number_of_nodes()
+        self.node_list = list(G.nodes)
+        return A, nodes
+
+
+    def probability_matrix(self, A):
+        # A[:, 1] : second column
+        # len(A[:,1]) <-- size of column
+        
         for x in range(A.shape[1]):
-            #counting number of page occurence
-            count = np.count_nonzero(A == 1, axis = 0)
-            #print(count)
-            
+            # counting number of page occurrence
+            count = np.count_nonzero(A == 1, axis=0)
+            # print(count)
+
         for row in range(len(A)):
             for column in range(len(A[row])):
-                print("matrix value", A[row,column])
-                print("count", count[column])
-                if (count[column] != 0):
-                    A[row,column] /= count[column]
-                
+                # print("matrix value", A[row, column])
+                # print("count", count[column])
+                if count[column] != 0:
+                    A[row, column] /= count[column]
+
         return A
-            
-        #numpy.savetxt("prob_matrix.csv", A, delimiter=",")
     
-    def get_pageRank(matrix, v):
+    
+    def get_pageRank(self, matrix, v):
         # First iteration:
         #     matrix        v      result
         # |[0.33 0.5 0]|   |v0|   |new_v0|
         # |[0.33  0  0]| x |v1| = |new_v1|
         # |[0.33 0.5 1]|   |v2|   |new_v2|
-        
+
         # Second iteration:
         #     matrix           matrix        v      result
         # |[0.33 0.5 0]|   |[0.33 0.5 0]|   |v0|   |new_v0|
@@ -66,23 +80,31 @@ class pageRank:
         # |[0.33 0.5 1]|   |[0.33 0.5 1]|   |v2|   |new_v2|
         #                  ^ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
         #                 result from first iteration
-        
         result = v
-        
+
         # iterating until it converges
         while True:
             # current v = previous result
             v = result
             # calculate new result
             result = matrix.dot(v)
-            
+
             # if result doesn't change much, break the loop
-            if np.subtract(result, v).all() <= 0.0001:
+            if all(np.subtract(result, v) <= 0.000001):
                 break
-        
+
         return result
-   
     
+    
+    def get_top_100(self):
+        page = {"page{}".format(ID+1): self.pr[ID][0] for ID in range(len(self.node_list))}            
+        sort_pages = sorted(page.items(), key=lambda x: x[1], reverse=True)
+        
+        for item in sort_pages[:100]:
+            print(item[0], item[1])
+        
+        
+
 def create_edge_list(seed_edges):
     index = 0
     for node_sets in seed_edges:
